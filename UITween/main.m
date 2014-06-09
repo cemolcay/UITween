d91ad7daab0bb89ad0a44cc918c2771023c0ab0f
cemolcay <ccemolcay@gmail.com>
2014-06-09 11:43:40 +0300

    Initial Commit

---
+++ UITween/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITween
+//
+//  Created by Cem Olcay on 09/06/14.
+//  Copyright (c) 2014 studionord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
